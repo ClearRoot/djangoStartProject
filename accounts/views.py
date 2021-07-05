90f4af35da74f552bd669c28979d20b1a2e5ba13
Jo Hogeun <jhk7767@gmail.com>
2021-07-05 11:31:44 +0900

    7/5 class_6 commit 1

--- accounts/views.py
+++ accounts/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def hello_page(request):
-    return render(request, 'base.html')
+    return render(request, 'accounts/hello_world.html')
