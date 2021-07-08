cba830825fd72d797cb0edbac0f7177fcfb4fa39
Jo Hogeun <jhk7767@gmail.com>
2021-07-08 12:53:12 +0900

    Modify hello_world page

--- accounts/views.py
+++ accounts/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def hello_page(request):
-    return render(request, 'accounts/hello_world.html')
+    if request.method == 'POST':
+        return render(request, 'accounts/hello_world.html',
+                      context={'text': 'POST METHOD'})
+    else:
+        return render(request, 'accounts/hello_world.html',
+                      context={'text': 'GET METHOD'})
