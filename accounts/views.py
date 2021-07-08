71926cdf2676181c2c5b34d72c9d0d88b38463a8
Jo Hogeun <jhk7767@gmail.com>
2021-07-08 12:36:45 +0900

    7/8 class_6 commit 3

--- accounts/views.py
+++ accounts/views.py
@@ -4,4 +4,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def hello_page(request):
-    return render(request, 'accounts/hello_world.html')
+    if request.method == 'POST':
+        return render(request, 'accounts/hello_world.html',
+                      context={'text': 'POST METHOD'})
+    else:
+        return render(request, 'accounts/hello_world.html',
+                      context={'text': 'GET METHOD'})
