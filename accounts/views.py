6366992696cbb617bf6eeb102800bfb4b223131a
Jo Hogeun <jhk7767@gmail.com>
2021-07-12 12:30:25 +0900

    Modify accounts views.py
    HttpResponseRedirect 와 urls.reverse를 사용

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from accounts.models import HelloWorld
 
 
@@ -11,10 +13,7 @@ def hello_page(request):
         new_hello_world.text = temp
         new_hello_world.save()
 
-        hello_world_list = HelloWorld.objects.all()
-
-        return render(request, 'accounts/hello_world.html',
-                      context={'hello_world_list': hello_world_list})
+        return HttpResponseRedirect(reverse('accounts:hello'))
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accounts/hello_world.html',
