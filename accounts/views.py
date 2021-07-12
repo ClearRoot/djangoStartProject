88a5330ee01642e1056ba839432a81c0c9a97709
Jo Hogeun <jhk7767@gmail.com>
2021-07-12 11:31:29 +0900

    7/12 class_6 commit 2

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from accounts.models import HelloWorld
 
 
 # Create your views here.
@@ -8,8 +8,12 @@ def hello_page(request):
 
         temp = request.POST.get('input_text')
 
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
         return render(request, 'accounts/hello_world.html',
-                      context={'text': temp})
+                      context={'new_hello_world': new_hello_world})
     else:
         return render(request, 'accounts/hello_world.html',
                       context={'text': 'GET METHOD'})
