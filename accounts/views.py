9fba0f0a6522d8f1b8466d7813a860cc1bee2ec1
Jo Hogeun <jhk7767@gmail.com>
2021-07-12 11:42:12 +0900

    Modify accounts views

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
+from accounts.models import HelloWorld
 
 
 # Create your views here.
 def hello_page(request):
     if request.method == 'POST':
         temp = request.POST.get('input_text')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
         return render(request, 'accounts/hello_world.html',
-                      context={'text': temp})
+                      context={'new_hello_world': new_hello_world})
     else:
         return render(request, 'accounts/hello_world.html',
                       context={'text': 'GET METHOD'})
