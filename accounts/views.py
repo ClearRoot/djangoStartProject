6a9085d290d6dea571156a55c0910f43ed3e7010
Jo Hogeun <jhk7767@gmail.com>
2021-07-13 17:47:02 +0900

    Create template
    Create sing up page

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView
-from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from accounts.models import HelloWorld
 
@@ -25,6 +25,6 @@ def hello_page(request):
 
 class AccountCreateView(CreateView):
     model = User
-    form_class = UserChangeForm
+    form_class = UserCreationForm
     success_url = reverse_lazy('accounts:hello')
     template_name = 'accounts/create.html'
