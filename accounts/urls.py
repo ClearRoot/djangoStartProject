86249ee431d13df7d6a1a6b8db89a0eb6d3e5684
Jo Hogeun <jhk7767@gmail.com>
2021-07-15 12:44:29 +0900

    7/15 class_6 commit 4

--- accounts/urls.py
+++ accounts/urls.py
@@ -16,13 +16,16 @@ Including another URLconf
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from accounts import views
-from accounts.views import AccountCreateView
+from accounts.views import AccountCreateView, AccountDetailView
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('hello/', views.hello_page, name='hello'),
-    path('create/', AccountCreateView.as_view(), name='create'),
+
     path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+
+    path('create/', AccountCreateView.as_view(), name='create'),
+    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from accounts.models import HelloWorld
 
@@ -31,3 +31,9 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('accounts:hello')
     template_name = 'accounts/create.html'
+
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accounts/detail.html'
