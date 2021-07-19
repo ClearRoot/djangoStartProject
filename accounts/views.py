7f4f1e81cee565b9a541789c940920b1819a44f8
Jo Hogeun <jhk7767@gmail.com>
2021-07-19 11:28:48 +0900

    7/19 class_6 commit 1

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView
 
 from accounts.models import HelloWorld
 
@@ -37,3 +37,11 @@ class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accounts/detail.html'
+
+
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = UserCreationForm
+    context_object_name = 'target_user'
+    success_url = reverse_lazy('accounts:hello')
+    template_name = 'accounts/update.html'
