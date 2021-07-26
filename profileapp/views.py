691aa759bed2dfd31ad03d18173ac9d0f87fdce9
Jo Hogeun <jhk7767@gmail.com>
2021-07-26 12:24:18 +0900

    7/26 class_6 commit 3

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from profileapp.forms import ProfileCreationForm
+from profileapp.models import Profile
 
 
-# Create your views here.
+class ProfileCreateView(CreateView):
+    model = Profile
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('profileapp:create')
+    template_name = 'profileapp/create.html'
