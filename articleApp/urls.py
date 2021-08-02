cacf48891a70833adeca77dc8075034b5a1b31f1
Jo Hogeun <jhk7767@gmail.com>
2021-08-02 11:34:36 +0900

    Add magic grid function

--- articleApp/urls.py
+++ articleApp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
+from django.views.generic import TemplateView
 
 app_name = 'articleApp'
 
 urlpatterns = [
-    path(),
+    path('list/', TemplateView.as_view(template_name='articleApp/list.html'), name='list'),
 ]
