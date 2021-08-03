0040e427ca3d4d3ecc668909b6f9941de1adbba6
Jo Hogeun <jhk7767@gmail.com>
2021-08-03 17:19:54 +0900

    Create article detail page

--- articleApp/urls.py
+++ articleApp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from articleApp.views import ArticleCreateView
+from articleApp.views import ArticleCreateView, ArticleDetailView
 
 app_name = 'articleApp'
 
 urlpatterns = [
     path('list/', TemplateView.as_view(template_name='articleApp/list.html'), name='list'),
     path('create/', ArticleCreateView.as_view(), name='create'),
+    path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail'),
 ]
