2344acb14b2862894d34f84812bbc79ce99ba934
Jo Hogeun <jhk7767@gmail.com>
2021-08-03 16:55:26 +0900

    Create article create page

--- articleApp/urls.py
+++ articleApp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+from articleApp.views import ArticleCreateView
+
 app_name = 'articleApp'
 
 urlpatterns = [
     path('list/', TemplateView.as_view(template_name='articleApp/list.html'), name='list'),
+    path('create/', ArticleCreateView.as_view(), name='create'),
 ]
