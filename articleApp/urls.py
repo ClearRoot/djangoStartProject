81a924e84051422ea11ca413f77da447d9030fc2
Jo Hogeun <jhk7767@gmail.com>
2021-08-03 17:55:36 +0900

    Create article update page

--- articleApp/urls.py
+++ articleApp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from articleApp.views import ArticleCreateView, ArticleDetailView
+from articleApp.views import ArticleCreateView, ArticleDetailView, ArticleUpdateView
 
 app_name = 'articleApp'
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('list/', TemplateView.as_view(template_name='articleApp/list.html'), name='list'),
     path('create/', ArticleCreateView.as_view(), name='create'),
     path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail'),
+    path('update/<int:pk>', ArticleUpdateView.as_view(), name='update'),
 ]
