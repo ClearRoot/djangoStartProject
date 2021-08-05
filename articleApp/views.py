866b5f5bb12710c80d193b32b3ffd6666b83be65
Jo Hogeun <jhk7767@gmail.com>
2021-08-05 11:16:40 +0900

    8/5 class_6 commit 1

--- articleApp/views.py
+++ articleApp/views.py
@@ -1,20 +1,27 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from articleApp.decorators import article_ownership_required
 from articleApp.forms import ArticleCreationForm
 from articleApp.models import Article
 
 
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class ArticleCreateView(CreateView):
     model = Article
     form_class = ArticleCreationForm
-    success_url = reverse_lazy('articleApp:list')
     template_name = 'articleApp/create.html'
 
     def form_valid(self, form):
         form.instance.writer = self.request.user
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('articleApp:detail', kwargs={'pk': self.object.pk})
 
 
 class ArticleDetailView(DetailView):
@@ -23,6 +30,8 @@ class ArticleDetailView(DetailView):
     template_name = 'articleApp/detail.html'
 
 
+@method_decorator(article_ownership_required, 'get')
+@method_decorator(article_ownership_required, 'post')
 class ArticleUpdateView(UpdateView):
     model = Article
     form_class = ArticleCreationForm
@@ -33,6 +42,8 @@ class ArticleUpdateView(UpdateView):
         return reverse('articleApp:detail', kwargs={'pk': self.object.pk})
 
 
+@method_decorator(article_ownership_required, 'get')
+@method_decorator(article_ownership_required, 'post')
 class ArticleDeleteView(DeleteView):
     model = Article
     context_object_name = 'target_article'
