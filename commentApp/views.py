04f0b8d215edf61f22ff5e211211503435adeac3
Jo Hogeun <jhk7767@gmail.com>
2021-08-09 11:47:53 +0900

    Create comment create page

--- commentApp/views.py
+++ commentApp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic import CreateView
 
-# Create your views here.
+from commentApp.forms import CommentCreationForm
+from commentApp.models import Comment
+
+
+class CommentCreateView(CreateView):
+    model = Comment
+    form_class = CommentCreationForm
+    template_name = 'commentApp/create.html'
+
+    def get_success_url(self):
+        return reverse('articleApp:detail', kwargs={'pk': self.object.article.pk})
