d11c429f96f3536c753c0dc66dd5c90219a8578e
Jo Hogeun <jhk7767@gmail.com>
2021-08-09 12:38:49 +0900

    8/9 class_6 commit 5

--- commentApp/views.py
+++ commentApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DeleteView
 
 from commentApp.forms import CommentCreationForm
 from commentApp.models import Comment
@@ -15,6 +15,15 @@ class CommentCreateView(CreateView):
         form.instance.writer = self.request.user
         form.instance.article_id = self.request.POST.get('article_pk')
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('articleApp:detail', kwargs={'pk': self.object.article.pk})
+
+
+class CommentDeleteView(DeleteView):
+    model = Comment
+    context_object_name = 'target_comment'
+    template_name = 'commentApp/delete.html'
 
     def get_success_url(self):
         return reverse('articleApp:detail', kwargs={'pk': self.object.article.pk})
