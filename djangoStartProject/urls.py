d81be3461e9b52fd06de92602b092ee684d7dbf7
Jo Hogeun <jhk7767@gmail.com>
2021-08-09 11:20:20 +0900

    Create comment app

--- djangoStartProject/urls.py
+++ djangoStartProject/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('profiles/', include('profileapp.urls')),
     path('articles/', include('articleApp.urls')),
+    path('comments/', include('commentApp.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
