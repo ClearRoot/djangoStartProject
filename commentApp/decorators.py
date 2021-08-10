cb58fd8e6a8c0a0d49e44cdd6a9bed5f3771d87a
Jo Hogeun <jhk7767@gmail.com>
2021-08-10 17:44:52 +0900

    Bug fix

--- commentApp/decorators.py
+++ commentApp/decorators.py
@@ -7,7 +7,7 @@ def comment_ownership_required(func):
     def decorated(request, *args, **kwargs):
         target_comment = Comment.objects.get(pk=kwargs['pk'])
         if target_comment.writer == request.user:
-            return func()
+            return func(request, *args, **kwargs)
         else:
             return HttpResponseForbidden()
     return decorated
