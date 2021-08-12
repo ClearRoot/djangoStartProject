43a365b49c65e1bc1c3c782a31d71c4609b7328e
Jo Hogeun <jhk7767@gmail.com>
2021-08-12 12:45:34 +0900

    8/12 class_6 commit 7

--- projectApp/views.py
+++ projectApp/views.py
@@ -1,16 +1,22 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView
 
 from projectApp.forms import ProjectCreationForm
 from projectApp.models import Project
 
 
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class ProjectCreateView(CreateView):
     model = Project
     form_class = ProjectCreationForm
-    success_url = reverse_lazy('articleApp:list')
     template_name = 'projectApp/create.html'
+
+    def get_success_url(self):
+        return reverse('projectApp:detail', kwargs={'pk': self.object.pk})
 
 
 class ProjectDetailView(DetailView):
