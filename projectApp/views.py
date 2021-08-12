7e9bf772653dd4274ae919ebae3786e961f9c113
Jo Hogeun <jhk7767@gmail.com>
2021-08-12 12:26:03 +0900

    Create project create page

--- projectApp/views.py
+++ projectApp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from projectApp.forms import ProjectCreationForm
+from projectApp.models import Project
+
+
+class ProjectCreateView(CreateView):
+    model = Project
+    form_class = ProjectCreationForm
+    success_url = reverse_lazy('articleApp:list')
+    template_name = 'projectApp/create.html'
