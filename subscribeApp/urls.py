2c6b10f058ada7aa20809d615a01c0f630be4d26
Jo Hogeun <jhk7767@gmail.com>
2021-08-23 11:24:38 +0900

    8/23 class_6 commit 1

--- subscribeApp/urls.py
+++ subscribeApp/urls.py
@@ -1,4 +1,9 @@
+from django.urls import path
+
+from subscribeApp.views import SubscriptionView
+
+app_name = 'subscribeApp'
 
 urlpatterns = [
-
+    path('subscribe/<int:project_pk>', SubscriptionView.as_view(), name='subscribe'),
 ]
