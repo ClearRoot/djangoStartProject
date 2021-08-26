fd0b1af0699388b8dd7f00ca7d9d1646375bd9d9
Jo Hogeun <jhk7767@gmail.com>
2021-08-26 12:48:17 +0900

    Delete hello page

--- accounts/urls.py
+++ accounts/urls.py
@@ -15,14 +15,11 @@ Including another URLconf
 """
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-from accounts import views
 from accounts.views import AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('hello/', views.hello_page, name='hello'),
-
     path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 
